1b5d3d007e5a568fd15a85896a45de0d3a357609
Niaiko22 <ralaivaoniaiko@gmail.com>
2025-07-18 11:27:15 +0300

    Ajout de prefix 'v1' aux routes existants

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome', ['version' => '1.0']);
+Route::prefix('v1')->group(function() {
+    Route::get('/', function () {
+        return view('welcome', ['version' => '1.0']);
+    });
+
+    Route::get('/about', function () {
+        return 'À propos';
+    });
 });
 
-Route::get('/about', function () {
-    return 'À propos';
-});
